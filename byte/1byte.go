a72eb80b8920eabbbb5aa7de628b5c7212560534
sky-bai <569531262@qq.com>
2021-10-02 17:22:29 +0800

    网络中的字节

---
+++ byte/1byte.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// 字节是网络传输的基本单位 在内存和磁盘存储信息的单位
+// 一个英文占一个字节的大小 一个中文占两个字节的大小
+// 字符串转字节数组 字节数组每一个存的是每个字符的编码
+func main() {
+	bytes := []byte("hello")
+	fmt.Println(bytes)
+}
