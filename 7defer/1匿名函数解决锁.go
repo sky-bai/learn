08532c571d146042b60a406868fd94326eba2ac9
sky-bai <569531262@qq.com>
2021-11-01 19:22:59 +0800

    闭包解决锁问题

---
+++ 7defer/1匿名函数解决锁.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	sync2 "sync"
+)
+
+// defer 是在函数退出的时候进行执行的
+
+func main() {
+	sync := sync2.Mutex{}
+	for i := 0; i < 3; i++ {
+		sync.Lock()
+		defer sync.Unlock()
+		fmt.Println(1)
+	}
+}
+
+func main1() {
+	sync := sync2.Mutex{}
+	for i := 0; i < 3; i++ {
+		go func() {
+			sync.Lock()
+			defer sync.Unlock()
+			fmt.Println(2)
+		}()
+	}
+}
