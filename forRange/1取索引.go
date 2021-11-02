05dc196e542cf696852e520f5912c0bb9e7a9372
sky-bai <569531262@qq.com>
2021-11-02 09:28:45 +0800

    forrange遍历 通过数组下标改变数组之前的元素

--- forRange/1取索引.go
+++ forRange/1取索引.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	s1 := []int{10, 20, 30, 40, 50}
 	fmt.Println(s1[1:3])
-	for index, _ := range s1 {
+	for index := range s1 {
 		s1[index] *= 2
 	}
 	fmt.Printf("%v\n", s1)
