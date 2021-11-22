cb94cc07245bf30335ed3d46b3f57a808819d71f
sky-bai <569531262@qq.com>
2021-11-22 10:05:00 +0800

    map 序列化 转 json

---
+++ md5/map.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+)
+
+func main() {
+	paramMap := make(map[string]interface{})
+	paramMap["token"] = "SC-7z7Nk9q9me5V5uDfqLrKIbwBu6QdhXRg"
+	paramMap["signature"] = "9c0f96468da5bf1222b66978b0d2ff5d"
+
+	value, err := jsoniter.Marshal(paramMap)
+	if err != nil {
+
+		return
+	}
+	fmt.Println("111", paramMap)
+
+	paramMap1 := make(map[string]interface{})
+
+	err = jsoniter.Unmarshal(value, &paramMap1)
+	if err != nil {
+		return
+
+	}
+
+	fmt.Println("222", paramMap1)
+}
