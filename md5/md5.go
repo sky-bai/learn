e9c23226cabbc8e3e8cf3ac5256b4b3138f2289f
sky-bai <569531262@qq.com>
2021-11-22 10:05:18 +0800

    md5加密

---
+++ md5/md5.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func main() {
+	str := "cd_admin" + "c071248fa3e9bd2e6d07f7c2b2df87c3" + "d50413ce-8929-17fe-cb6e-e6c75c47e4f2"
+	st := fmt.Sprintf("%x", md5.Sum([]byte(str)))
+	fmt.Println(st)
+}
