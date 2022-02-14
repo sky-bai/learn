107fd28dbe9f40e06b160c81afe748ab71713ddb
sky-bai <569531262@qq.com>
2022-02-14 09:31:14 +0800

    for range 遍历切片后 只获取一个值，该值是索引下标

--- forRange/1取索引.go
+++ forRange/1取索引.go
@@ -6,6 +6,7 @@ func main() {
 	s1 := []int{10, 20, 30, 40, 50}
 	fmt.Println(s1[1:3])
 	for index := range s1 {
+		fmt.Println("for range 遍历切片后 只获取一个值，该值是索引下标", index)
 		s1[index] *= 2
 	}
 	fmt.Printf("%v\n", s1)
