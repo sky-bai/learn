635bfd75cdd443ea34855353a6a9e61a044d2755
sky-bai <569531262@qq.com>
2022-02-14 09:28:28 +0800

    这个函数本身要panic,就在这个函数内recover  这样就会使main函数继续执行

--- 17panic/1panic.go
+++ 17panic/1panic.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
+	f()
+	fmt.Println("Hello, World!")
+}
+func f() {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println("Recovered in f", err)
 		}
 	}()
-	f()
-	fmt.Println("Hello, World!")
-}
-func f() {
 	defer fmt.Println("defer f")
 	panic("PANIC")
 }
+
+// 这个函数本身要panic,就在这个函数内recover  这样就会使main函数继续执行
