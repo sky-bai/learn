a0e37ec650c483d8c7c2271e2b7c7f141901e6da
sky-bai <569531262@qq.com>
2022-02-15 15:52:20 +0800

    正则

--- 19valid/3match.go
+++ 19valid/3match.go
@@ -8,4 +8,6 @@ import (
 func main() {
 	matched, err := regexp.Match(`foo.*`, []byte(`seafod`))
 	fmt.Println(matched, err)
+	matched1, err := regexp.Match(`^[a-zA-Z0-9_]{6,128}$`, []byte("4512w34"))
+	fmt.Println(matched1, err)
 }
