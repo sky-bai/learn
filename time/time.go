e99fd30cf76bac39bccf1291f00d65c4fa6f5643
sky-bai <569531262@qq.com>
2022-05-07 21:48:50 +0800

    Go的time. Now()会设置时间为本地时区

--- time/time.go
+++ time/time.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/jinzhu/now"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,4 +13,13 @@ func main() {
 	endDay := now.New(oneMonth).EndOfMonth()
 	fmt.Println("", startDay)
 	fmt.Println("", endDay)
+
+	var TickerCount int64
+	fmt.Println("", TickerCount)
+
+	atomic.LoadInt64(&TickerCount) //获取数据 在这里读取value的值的同时，当前计算机中的任何CPU都不会进行其它的针对此值的读或写操作。
+	fmt.Println("", TickerCount)
+
+	atomic.StoreInt64(&TickerCount, 1)
+	fmt.Println("", TickerCount)
 }
