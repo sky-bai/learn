dae1c67e5cddc2e8a3141c37fb15196e6e866413
sky-bai <569531262@qq.com>
2022-11-09 21:57:05 +0800

    hystrix

---
+++ hystrix/request.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var client *http.Client
+
+func init() {
+	tr := &http.Transport{
+		MaxIdleConns:    100,
+		IdleConnTimeout: 1 * time.Second,
+	}
+	client = &http.Client{Transport: tr}
+}
+
+type info struct {
+	Data interface{} `json:"data"`
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 11; i++ {
+		wg.Add(1)
+		go func(int2 int) {
+			defer wg.Done()
+
+			req, err := http.NewRequest("GET", "http://127.0.0.1:8090", nil)
+			if err != nil {
+				fmt.Printf("初始化http客户端处错误:%v", err)
+				return
+			}
+			resp, err := client.Do(req)
+
+			if err != nil {
+				fmt.Printf("初始化http客户端处错误:%v", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			nByte, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Printf("读取http数据失败:%v", err)
+				return
+			}
+
+			fmt.Printf("第 %d 次请求,接收到到值:%v\n", int2+1, string(nByte))
+		}(i)
+	}
+	wg.Wait()
+	fmt.Printf("请求完毕\n")
+}
