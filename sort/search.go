fdab2b73b191d596b8a33bb1ec5f376e85faca32
sky-bai <569531262@qq.com>
2022-11-29 18:30:25 +0800

    位运算

--- sort/search.go
+++ sort/search.go
@@ -5,8 +5,57 @@ import (
 	"sort"
 )
 
+// lc40
 func main() {
-	a := []int{1, 2, 3, 4, 5}
-	b := sort.Search(len(a), func(i int) bool { return a[i] >= 30 })
-	fmt.Println(b)
+	first := []int{1, 2, 4, 8, 16}
+	// target := 24
+
+	sss := combinationSum2(first, 31)
+	fmt.Println(sss[0])
+
+	// 要求列出所有和为target的组合
+
 }
+func combinationSum2(candidates []int, target int) (ans [][]int) {
+	sort.Ints(candidates)
+	var freq [][2]int
+	for _, num := range candidates {
+		if freq == nil || num != freq[len(freq)-1][0] {
+			freq = append(freq, [2]int{num, 1})
+		} else {
+			freq[len(freq)-1][1]++
+		}
+	}
+
+	var sequence []int
+	var dfs func(pos, rest int)
+	dfs = func(pos, rest int) {
+		if rest == 0 {
+			ans = append(ans, append([]int(nil), sequence...))
+			return
+		}
+		if pos == len(freq) || rest < freq[pos][0] {
+			return
+		}
+
+		dfs(pos+1, rest)
+
+		most := min(rest/freq[pos][0], freq[pos][1])
+		for i := 1; i <= most; i++ {
+			sequence = append(sequence, freq[pos][0])
+			dfs(pos+1, rest-i*freq[pos][0])
+		}
+		sequence = sequence[:len(sequence)-most]
+	}
+	dfs(0, target)
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// 判断数组里面元素之间自由组合有相同的和
