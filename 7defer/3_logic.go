a79c6adb3a39308d4a09f37481a0bbf8932c2136
sky-bai <569531262@qq.com>
2023-03-30 15:30:47 +0800

    logic

---
+++ 7defer/3_logic.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	normalReturn := false // 先定义没有成功
+
+	// 那么成功了就设置成true
+	normalReturn = true
+	if normalReturn { //
+		println("normal return")
+	}
+
+	if !normalReturn {
+		println("no normal return")
+	}
+
+}
